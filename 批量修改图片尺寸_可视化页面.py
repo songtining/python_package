4bcf4a0849c55acf5da490b86549f523bf3bdf51
songtining <40161617+songtining@users.noreply.github.com>
2025-03-18 14:08:03 +0800

    Update 批量修改图片尺寸_可视化页面.py

--- 批量修改图片尺寸_可视化页面.py
+++ 批量修改图片尺寸_可视化页面.py
@@ -167,7 +167,7 @@ def process_images_in_folder(root_folder):
                         # convert_rgb_to_cmyk_jpeg(tif_image_path, os.path.splitext(image_path)[0] + ".jpg")
 
                         jpg_seq_str = str(jpg_seq) if jpg_seq > 10 else "0" + str(jpg_seq)
-                        jpg_file_name = "已修改" + jpg_seq_str + "(" + folder_name + ")" + ".jpg"
+                        jpg_file_name = os.path.join(current_folder, "已修改" + jpg_seq_str + "(" + folder_name + ")" + ".jpg")
                         convert_rgb_to_cmyk_jpeg(tif_image_path, jpg_file_name)
                         write_log(f"✅ 第四步：调用PS -> 图片转CMYK模式成功, 文件保存到本地成功...")
                         jpg_seq += 1
